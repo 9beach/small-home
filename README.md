21040a1e21631b083a6070eae0f768ef793e988e
9beach <9beach@gmail.com>
2023-04-14 05:48:02 +0900

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # small-home
+
 Guest account setting script without app installation.
+
+Follow the steps below at the first login as your guest account, then creates
+`.vimrc` and `.zshrc` or `.bashrc` for your guest account.
+
+```
+git clone git@github.com:9beach/small-home.git
+cd small-home
+bin/install.sh
+```
---
+++ bin/install.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+REPO_PATH="$(dirname $(cd "$(dirname "$0")" > /dev/null 2>&1; pwd -P))"
+
+set -x
+set -e
+
+case "$(uname -sr)" in
+	Darwin*)
+		PLATFORM='darwin'
+		;;
+	Linux*icrosoft*)
+		PLATFORM='wsl'
+		echo "Do not support $(uname -sr)." && exit 1
+		;;
+	Linux*)
+		PLATFORM='linux'
+		;;
+	*BSD*)
+		PLATFORM='bsd'
+		;;
+	*)
+		echo "Do not support $(uname -sr)." && exit 1
+		;;
+esac
+
+# Links to my profiles.
+cd
+touch .hushlogin
+
+if type zsh &> /dev/null; then
+	ln -s $REPO_PATH/rc/profile .zshrc
+	chsh -s $(which zsh)
+else
+	ln -s $REPO_PATH/rc/profile .bashrc
+fi
+
+ln -s $REPO_PATH/rc/writing-quotes .writing-quotes
+
+ln -s $REPO_PATH/rc/vimrc .vimrc
+if type nvim &> /dev/null; then
+	mkdir -p .config/nvim
+	ln -s $REPO_PATH/rc/vimrc .config/nvim/init.vim
+fi
+
+# Essential `vim` plugins.
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+if type nvim &> /dev/null; then
+	nvim --headless +PluginInstall +qall 2> /dev/null
+elif type vim &> /dev/null; then
+	vim -E -s -u ~/.vimrc +PlugInstall +qall 2> /dev/null
+fi
+
+# Essential zsh plugins.
+git clone https://github.com/sindresorhus/pure.git ~/.zsh/pure
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+git clone https://github.com/agkozak/zsh-z ~/.zsh/zsh-z
