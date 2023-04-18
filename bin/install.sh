330ad074768df7006e232db0600fd56b7cf97974
9beach <9beach@gmail.com>
2023-04-18 12:22:27 +0900

    Add `CP=yes` option to `install.sh`

--- bin/install.sh
+++ bin/install.sh
@@ -5,13 +5,18 @@ REPO_PATH="$(dirname $(cd "$(dirname "$0")" > /dev/null 2>&1; pwd -P))"
 set -x
 set -e
 
+if [ $CP = 'yes' ]; then
+	LN='cp -f'
+else
+	LN='ln -sf'
+fi
+
 case "$(uname -sr)" in
 	Darwin*)
 		PLATFORM='darwin'
 		;;
 	Linux*icrosoft*)
 		PLATFORM='wsl'
-		echo "Do not support $(uname -sr)." && exit 1
 		;;
 	Linux*)
 		PLATFORM='linux'
@@ -37,18 +42,18 @@ if type zsh &> /dev/null; then
 	git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 	git clone https://github.com/agkozak/zsh-z ~/.zsh/zsh-z
 
-	ln -s $REPO_PATH/rc/profile .zshrc
-	ln -s $REPO_PATH/rc/profile .bashrc
+	$LN $REPO_PATH/rc/profile .zshrc
+	$LN $REPO_PATH/rc/profile .bashrc
 	chsh -s $(which zsh)
 else
-	ln -s $REPO_PATH/rc/profile .bashrc
+	$LN $REPO_PATH/rc/profile .bashrc
 fi
 
-ln -s $REPO_PATH/rc/writing-quotes .writing-quotes
-ln -s $REPO_PATH/rc/vimrc .vimrc
+$LN $REPO_PATH/rc/writing-quotes .writing-quotes
+$LN $REPO_PATH/rc/vimrc .vimrc
 if type nvim &> /dev/null; then
 	mkdir -p .config/nvim
-	ln -s $REPO_PATH/rc/vimrc .config/nvim/init.vim
+	$LN $REPO_PATH/rc/vimrc .config/nvim/init.vim
 fi
 
 # Essential `vim` plugins.
@@ -59,3 +64,5 @@ if type nvim &> /dev/null; then
 elif type vim &> /dev/null; then
 	vim +PluginInstall +qall
 fi
+
+echo 'Install completed. Please logout and login again.'
